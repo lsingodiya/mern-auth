9b32124ec573e0b708b6100d2d2472feb55486ad
lavisingodiya <lavi.singodiya@raptbot.in>
2026-01-19 16:31:44 +0530

    Optimized for ECS

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 3000,
+    allowedHosts: [
+      'mern-app-1945846510.eu-north-1.elb.amazonaws.com',
+    ],
     proxy: {
       '/api': {
-        target: 'http://3.109.154.126:5000',
+        target: 'http://172.31.32.178:5000',
         changeOrigin: true,
       },
     },
